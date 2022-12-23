34e884f82ac918dd205df475e4acfe202cbcdc0e
Bordjiano <vanya.misyura@gmail.com>
2022-12-23 11:28:16 +0200

    Task 1_11(10)

--- js/task_1.11/tasks.js
+++ js/task_1.11/tasks.js
@@ -118,3 +118,13 @@ function task9(n){
     else
         console.log("-")
 }
+
+//  Task 10
+function task10(arr){
+    for (let i = 0; i < arr.length; i++){
+        if (typeof arr[i] !== "string" || typeof arr[i] !== "symbol" || typeof arr[i] !== "boolean" || typeof arr[i] !== "object"){
+            arr[i] = Math.pow(arr[i], 2)
+        }
+    }
+    console.log(arr)
+}
