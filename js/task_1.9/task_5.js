b9fc04aaab502a6ebd7de19375cb2f7f27953e66
Bordjiano <vanya.misyura@gmail.com>
2022-12-23 11:23:23 +0200

    Task 1_9(5)

---
+++ js/task_1.9/task_5.js
@@ -0,0 +1,9 @@
+let numb = 10_000
+let counter = 0
+do{
+    numb = numb / 2
+    counter++
+} while (numb > 50)
+const result = numb
+console.log("Result: " + result)
+console.log("Counter: " + counter)
